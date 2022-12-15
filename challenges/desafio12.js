98e6b2e4fc4032d1da7f73fce3364a94455a2c7e
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-12-15 16:51:17 -0300

    ✨ challenge - 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  {
+      $addToSet: { ingredientes: "ketchup" },
+  },
+  { upsert: true },
+);
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
